c8300233b164f237f20141b8c34acdfd6f5561ff
Valeriya Stange <valeriya.stange@gmail.com>
2025-02-13 22:17:33 +0100

    while + if

--- csript.js
+++ csript.js
@@ -6,14 +6,26 @@ function padRow(rowNumber, rowCount) {
     return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i + 1, count));
+for (let i = 1; i < count; i++) {
+    rows.push(padRow(i, count));
 }
 
 let result = "";
 
-for (const row of rows) {
-    result = result + row + "\n";
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+    done++;
+    rows.push(padRow(done, count));
+    if (done === count) {
+        continueLoop = false;
+    }
 }
+
+// TODO: use a different type of loop
+/* for (const row of rows) {
+    result = result + row + "\n";
+} */
 
 console.log(result);
